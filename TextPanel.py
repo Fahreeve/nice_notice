a395bf72a3cb8d761981e8bb17ddaa2dd1e69226
fahreeve <fahreeve@yandex.ru>
2014-02-11 20:37:15 +0400

    дописал класс textpanel, теперь окно выглядит как надо. Класс пока не создает событий

--- TextPanel.py
+++ TextPanel.py
@@ -1,52 +1,63 @@
+# -*- coding: utf-8 -*-
 import wx
-import TextCtrl
 
 class TextPanel(wx.Panel):
-    def __init__(self, parent, id, pos, size, style):
+    def __init__(self, parent, id, avatar, pos, size, style):
         wx.Panel.__init__(self, parent, id, pos, size, style)
-        self.size = size
         self.pos = pos
 	self.parent = parent
-	#self.bmp = save_photos(self.parent.userdata[0]["photo"], "C:\\Projects\\vk_messenger")
-    	#TextCtrl
-	bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
+	bSizer3 = wx.BoxSizer(wx.HORIZONTAL)
 		
-	self.m_bitmap1 = wx.StaticBitmap( self, wx.ID_ANY, wx.Bitmap( u"C:\\Projects\\vk_messenger\\2G3ZSYjqBWw.jpg", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
-	bSizer3.Add( self.m_bitmap1, 0, wx.ALL, 3 )
+	self.avatar = wx.StaticBitmap( self, wx.ID_ANY, avatar, wx.DefaultPosition, wx.DefaultSize, 0)
+	bSizer3.Add(self.avatar, 0, wx.ALL, 5)
 	
-	self.m_panel8 = TextCtrl.TextCtrl( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-	bSizer3.Add( self.m_panel8, 1, wx.EXPAND, 5 )
+	self.textctrl = wx.TextCtrl(self, wx.ID_ANY, size=wx.Size(235, -1), style=wx.TE_MULTILINE|wx.TE_NO_VSCROLL|wx.TE_RICH2|wx.BORDER_NONE) #|wx.TE_NOHIDESEL|
+	font = wx.Font(10, wx.MODERN, wx.NORMAL,wx.FONTWEIGHT_NORMAL, face="Tahoma")
+	self.textctrl.SetFont(font)	
 	
+	bSizer3.Add(self.textctrl, 1, wx.EXPAND|wx.TOP|wx.BOTTOM|wx.RIGHT, 5)
 	
-	self.SetSizer( bSizer3 )
-	self.Layout()	
-	self.bmp = self.CreateBacground()
-	dc = wx.ClientDC(self)
-	dc.DrawBitmap(self.bmp, 0, 0, True)
+	self.SetSizer(bSizer3)
+	self.Layout()
+	
+	self.SetBackgroundColour(wx.Color(255, 255, 255))
 	self.Bind(wx.EVT_PAINT, self.OnPaint)
-	self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-        
-        
-    def CreateBacground(self):
-        bmp = wx.EmptyBitmap(self.size.x, self.size.y)
-        memdc = wx.MemoryDC()
-        memdc.SelectObject(bmp)
-        memdc.SetBackground(wx.Brush(wx.Color(255, 255, 255)))
-        memdc.Clear()
-	
-	memdc.SetPen(wx.Pen(wx.Color(63, 63, 63), 1, wx.SOLID))
-	memdc.DrawPoint(0, 0)
-	memdc.DrawPoint(self.size.x - 1, self.size.y - 1)
-	memdc.DrawPoint(self.size.x - 1, 0)
-	memdc.DrawPoint(0, self.size.y - 1)
-	
-	#memdc.DrawBitmap(self.bmp, 4, 4, True)
-        return bmp
 
     def OnPaint(self, evt):
         dc = wx.PaintDC(self)
-        dc.DrawBitmap(self.bmp, 0,0, True)
+	dc.SetPen(wx.Pen(wx.Color(63, 63, 63), 1, wx.SOLID))
+	size = self.GetSize()
+	dc.DrawPoint(0, 0)
+	dc.DrawPoint(size.x - 1, size.y - 1)
+	dc.DrawPoint(size.x - 1, 0)
+	dc.DrawPoint(0, size.y - 1)	
 	
-    def OnLeftUp(self, evt):
-	#self.parent.OnClose()
-	pass
+    
+if  __name__ ==  "__main__":  
+    class TestFrame(wx.Frame):
+	def __init__(self, parent):
+	    wx.Frame.__init__(self, parent, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size(412, 100), wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL)
+	    
+	    self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
+	    
+	    bSizer1 = wx.BoxSizer(wx.VERTICAL)
+	    
+	    self.textpanel = TextPanel(self, wx.ID_ANY, wx.Bitmap( u"C:\\Projects\\vk_messenger\\2G3ZSYjqBWw.jpg", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.Size(300, 45), wx.TAB_TRAVERSAL)    
+	    bSizer1.Add(self.textpanel, 0, wx.EXPAND, 0)	
+	    
+	    self.SetSizer(bSizer1)
+	    self.Layout()
+	    
+	    self.Centre(wx.BOTH)    
+		    
+    
+    class MyApp(wx.App):
+	def OnInit(self):
+	    self.testframe = TestFrame(None)    
+	    self.testframe.Show(True)  
+	    self.SetTopWindow(self.testframe)
+	    return True
+    
+    app = MyApp(0)
+    app.MainLoop()           
+    
